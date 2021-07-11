1b898bc63313a90dcafabcb80b50dbd95cfc4e25
Merenalis <70169071+Merenalis@users.noreply.github.com>
2021-07-11 23:02:57 +0300

    Update formatting.js

--- scripts/formatting.js
+++ scripts/formatting.js
@@ -1,24 +1,12 @@
+const format = require('../scripts/format');
+
 el = document.getElementById('textarea');
 el.addEventListener('input', func_global);
-
 function func_global() {
     let text = document.getElementById('textarea').value;
     let printBlock = document.getElementById("print-global__print");
     printBlock.textContent = '';
-    printBlock.textContent += func(text);
 
-    function func(text) {
-        let result, result_tmp = '';
-        result_tmp = text.replace(new RegExp('([^a-zа-я])([^_]+)', 'gi'), (match, match2, match3) => {
+    printBlock.textContent += format(text);
 
-            match3 = match3[0].toUpperCase() + match3.slice(1);
-            return match2 + match3
-        });
-        result = result_tmp.replace(new RegExp('[_]', 'g'), '')
-
-        return result;
-
-
-    }
 }
-
